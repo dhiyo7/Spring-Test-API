b99d9bd15a863d74a311a8cbac060431b967bafe
dhiyo7 <dhiyoreksa12@gmail.com>
2021-11-10 16:51:22 +0700

    Initial Project

---
+++ src/test/java/com/dhiyo/restfullAPI/RestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhiyo.restfullAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
